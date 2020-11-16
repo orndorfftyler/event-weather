2d27e2b71edf90cc71e38dc7a191135febd295fe
Tyler Orndorff <orndorfftyler801@gmail.com>
2020-11-16 14:37:48 -0500

    getting reply from OW API

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-const searchURL = 'https://app.ticketmaster.com/discovery/v2/events.json';
+const searchURL_TM = 'https://app.ticketmaster.com/discovery/v2/events.json';
 
-const api_key = 'I3s7HGdzn7Y33zNiigtvz7tnmXdNJqMV';
+const apiKeyTM = 'I3s7HGdzn7Y33zNiigtvz7tnmXdNJqMV';
+
+const searchURL_OW = 'https://api.openweathermap.org/data/2.5/onecall';
+
+const apiKeyOW = 'ca33e47fabc12863ac34ba82ffb21974';
+
 
 // not using options??
 const options = {
@@ -15,13 +20,15 @@ for (var pair of options.headers.entries()) {
  
 // use this display function 
 
+// -------------------------------------------- Begin event management functions
+
 function displayEvents(responseJson) {
     $('.removable').remove();
     console.log(typeof responseJson);
     console.log(responseJson);
     //if (responseJson.limit != 0) {
         for (let i = 0; i < responseJson._embedded.events.length; i++) {
-            $('.results-list').append(`<li class="removable"><p>${responseJson._embedded.events[i].name}</p>
+            $('.results-list').append(`<li id="${cuid()}" class="removable"><p>${responseJson._embedded.events[i].name}</p>
             <p> Date: ${responseJson._embedded.events[i].dates.start.localDate}</p>
             <p> Venue: ${responseJson._embedded.events[i]._embedded.venues[0].name}</p>
             <p> Address: ${responseJson._embedded.events[i]._embedded.venues[0].address.line1}</p>
@@ -35,25 +42,22 @@ function displayEvents(responseJson) {
     }
 }
 
-//             <p> Info: ${responseJson._embedded.events[i].pleaseNote}</p>
-
-
 function paramFormat(params) {
     const queryItems = Object.keys(params)
-    .map(key => `${key}=${params[key]}`);
-    return(queryItems.join('&'));
-}
-
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+  }
+  
 function getEvents(eInput, sInput) { 
 
     let params = {
         keyword: eInput,
         stateCode: sInput,
-        apikey: api_key
+        apikey: apiKeyTM
       };
     
     let prettyParams = paramFormat(params);
-    const url = `${searchURL}?${prettyParams}`;
+    const url = `${searchURL_TM}?${prettyParams}`;
     console.log(url);
 
     //const url = `${searchURL}${eInput}/repos`; 
@@ -88,20 +92,76 @@ function search() {
     });
 }
 
+// -------------------------------------------- End event management functions
+
+// -------------------------------------------- Begin weather management functions
+
+function displayWeather(responseJson) {
+    $('.removable').remove();
+    console.log(typeof responseJson);
+    console.log(responseJson);
+    //if (responseJson.limit != 0) {
+        for (let i = 0; i < responseJson._embedded.events.length; i++) {
+            $('.results-list').append(`<li id="${cuid()}" class="removable"><p>${responseJson._embedded.events[i].name}</p>
+            <p> Date: ${responseJson._embedded.events[i].dates.start.localDate}</p>
+            <p> Venue: ${responseJson._embedded.events[i]._embedded.venues[0].name}</p>
+            <p> Address: ${responseJson._embedded.events[i]._embedded.venues[0].address.line1}</p>
+            <p> ${responseJson._embedded.events[i]._embedded.venues[0].city.name}, ${responseJson._embedded.events[i]._embedded.venues[0].state.name} ${responseJson._embedded.events[i]._embedded.venues[0].postalCode}</p>
+            <p class="latlong"> Lat: <b class="lat">${responseJson._embedded.events[i]._embedded.venues[0].location.latitude}</b> Long: <b class="long">${responseJson._embedded.events[i]._embedded.venues[0].location.longitude}</b></p>
+            
+            <button type="submit" class="event">Get Weather Forecast</button></li>
+            `);
+    //    }
+    $('.results').removeClass('hidden');
+    }
+}
+
+function getWeather(lat, long, itemID) { 
+
+    let params = {
+        lat: lat,
+        lon: long,
+        exclude: 'current,minutely,hourly',
+        appid: apiKeyOW
+      };
+    
+    let prettyParams = paramFormat(params);
+    const url = `${searchURL_OW}?${prettyParams}`;
+    console.log(url);
+
+    //const url = `${searchURL}${eInput}/repos`; 
+
+    fetch(url)
+    .then(response => {
+        if (response.ok) {
+            
+            if (!$('.error-message').hasClass('hidden')) {
+                $('.error-message').addClass('hidden');            
+            }
+            return response.json();
+        } else {
+            throw new Error(response.statusText);
+        }
+    })
+    .then(responseJson => displayWeather(responseJson))
+    .catch(error => {$('.error-message').text(`Something went wrong getting weather: ${error.message}`);
+    $('.error-message').removeClass('hidden')});
+
+}
+
 function weather() {
     $('ul').on('click',"button[class='event']", event => {
         event.preventDefault();
         let lat = $(event.currentTarget).prev().children('.lat').text();
         let long = $(event.currentTarget).prev().children('.long').text();
-        console.log(lat + long);
-        /*
-        let sInput = $('#sInput').val();
-        console.log(eInput + ' ' + sInput);
-        getEvents(eInput, sInput);
-        */
+        let itemID = $(event.currentTarget).parent().attr('id');
+        console.log(lat + long + itemID);
+        getWeather(lat,long,itemID);
     
     });
 }
+
+// -------------------------------------------- End event management functions
 
 function masterFunction() {
     search();
