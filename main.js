c3f5048a8bd37cf2f61ac6d604b266543abf69cf
Tyler Orndorff <orndorfftyler801@gmail.com>
2020-11-17 19:16:48 -0500

    fixed dayIndex calc so correct weather retrieved

--- main.js
+++ main.js
@@ -6,37 +6,31 @@ const searchURL_OW = 'https://api.openweathermap.org/data/2.5/onecall';
 
 const apiKeyOW = 'ca33e47fabc12863ac34ba82ffb21974';
 
-
-// not using options??
-const options = {
-    headers: new Headers({
-    'accept':'application/vnd.github.v3+json'})
-};
-/*
-for (var pair of options.headers.entries()) {
-    console.log(pair[0]+ ': '+ pair[1]);
- }
- */
- 
-// use this display function 
-
 // -------------------------------------------- Begin event management functions
 
 function displayEvents(responseJson) {
     $('.removable').remove();
-    console.log(typeof responseJson);
-    console.log(responseJson);
-    for (let i = 0; i < responseJson._embedded.events.length; i++) {
-        $('.results-list').append(`<li id="${cuid()}" class="removable container"><div><p>${responseJson._embedded.events[i].name}</p>
-        <p class="date"> Date: <b>${responseJson._embedded.events[i].dates.start.localDate}</b></p>
-        <p> Venue: ${responseJson._embedded.events[i]._embedded.venues[0].name}</p>
-        <p> Address: ${responseJson._embedded.events[i]._embedded.venues[0].address.line1}</p>
-        <p> ${responseJson._embedded.events[i]._embedded.venues[0].city.name}, ${responseJson._embedded.events[i]._embedded.venues[0].state.name} ${responseJson._embedded.events[i]._embedded.venues[0].postalCode}</p>
-        <p> Lat: <b class="lat">${responseJson._embedded.events[i]._embedded.venues[0].location.latitude}</b> Long: <b class="long">${responseJson._embedded.events[i]._embedded.venues[0].location.longitude}</b></p>
-        
-        <button type="submit" class="event">Get Weather Forecast</button></div></li>
-        `);
-    $('.results').removeClass('hidden');
+    console.log(responseJson.page.totalElements);
+    if (responseJson.page.totalElements > 0) {
+        console.log(typeof responseJson);
+        console.log(responseJson);
+        for (let i = 0; i < responseJson._embedded.events.length; i++) {
+            $('.results-list').append(`<li id="${cuid()}" class="removable container"><div><p>${responseJson._embedded.events[i].name}</p>
+            <p class="date"> Date: <b>${responseJson._embedded.events[i].dates.start.localDate}</b></p>
+            <p> Venue: ${responseJson._embedded.events[i]._embedded.venues[0].name}</p>
+            <p> Address: ${responseJson._embedded.events[i]._embedded.venues[0].address.line1}</p>
+            <p> ${responseJson._embedded.events[i]._embedded.venues[0].city.name}, ${responseJson._embedded.events[i]._embedded.venues[0].state.name} ${responseJson._embedded.events[i]._embedded.venues[0].postalCode}</p>
+            <p> Lat: <b class="lat">${responseJson._embedded.events[i]._embedded.venues[0].location.latitude}</b> Long: <b class="long">${responseJson._embedded.events[i]._embedded.venues[0].location.longitude}</b></p>
+            
+            <button type="submit" class="event">Get Weather Forecast</button>
+            </div></li>`);
+        $('.results').removeClass('hidden');
+        }
+    } else {
+        $('.results-list').append(`<li id="${cuid()}" class="removable container"><div>
+        <p>No results for this search</p>
+        </div></li>`);
+        $('.results').removeClass('hidden');
     }
 }
 
@@ -58,8 +52,6 @@ function getEvents(eInput, sInput) {
     const url = `${searchURL_TM}?${prettyParams}`;
     console.log(url);
 
-    //const url = `${searchURL}${eInput}/repos`; 
-
     fetch(url)
     .then(response => {
         if (response.ok) {
@@ -75,18 +67,19 @@ function getEvents(eInput, sInput) {
     .then(responseJson => displayEvents(responseJson))
     .catch(error => {$('.error-message').text(`Something went wrong getting events: ${error.message}`);
     $('.error-message').removeClass('hidden')});
-
 }
 
 function search() {
     $('form').on('click',"button[class='search']", event => {
         event.preventDefault();
-        let eInput = $('#eInput').val();
         let sInput = $('#state-select').val();
-        console.log(eInput + ' ' + sInput);
+        let eInput = $('#eInput').val().split(' ');
+        if (eInput.length > 6) { // cap search input at 6 words
+            eInput.splice(6);
+        }
+        eInput = eInput.join(',');
+        console.log(typeof eInput + eInput + ' ' + sInput);
         getEvents(eInput, sInput);
-
-
     });
 }
 
@@ -94,38 +87,26 @@ function search() {
 
 // -------------------------------------------- Begin weather management functions
 
-function hideWeather() {
-    $('ul').on('click',"button[class='wHide']", event => {
-        event.preventDefault();
-        $(event.currentTarget).parent().remove();
-    });
-
-}
-
-
-function displayWeather(responseJson,itemID) {
-    //$('.removable').remove();
+function displayWeather(responseJson,itemID,day) {
     console.log(typeof responseJson);
     console.log(responseJson);
-    //let targetDate = dateCalc(date);
-
-    let date = new Date(responseJson.daily[0].dt*1000).toISOString().slice(0,10);
+    /*
+    the day parameter will be the value of dayIndex which is calculated in function check7
+    see function check7 comments for details
+    */
     $(`li[id=${itemID}]`).append(`<div class="removable weather">
-    <p>Date: ${date}</p>
-    <img src="http://openweathermap.org/img/wn/${responseJson.daily[0].weather[0].icon}@2x.png" alt="weather icon matching description">
-    <p> Weather: ${responseJson.daily[0].weather[0].description} </p>
-    <p> Max Temp: ${responseJson.daily[0].temp.max}</p>
-    <p> Min Temp: ${responseJson.daily[0].temp.min}</p>
-    <p> Wind Speed: ${responseJson.daily[0].wind_speed} mph</p>
+    <p>${day}</p>
+    <img src="http://openweathermap.org/img/wn/${responseJson.daily[day].weather[0].icon}@2x.png" alt="weather icon matching description">
+    <p> Weather: ${responseJson.daily[day].weather[0].description} </p>
+    <p> Max Temp: ${responseJson.daily[day].temp.max}</p>
+    <p> Min Temp: ${responseJson.daily[day].temp.min}</p>
+    <p> Wind Speed: ${responseJson.daily[day].wind_speed} mph</p>
     
     <button type="submit" class="wHide">Hide</button>
     </div>`);
-    //$('.results').removeClass('hidden');
-    
 }
 
-
-function getWeather(lat, long, itemID, date) { 
+function getWeather(lat, long, itemID, day) { 
 
     let params = {
         lat: lat,
@@ -134,13 +115,10 @@ function getWeather(lat, long, itemID, date) {
         units: 'imperial',
         appid: apiKeyOW
       };
-    
 
     let prettyParams = paramFormat(params);
     const url = `${searchURL_OW}?${prettyParams}`;
     console.log(url);
-
-    //const url = `${searchURL}${eInput}/repos`; 
 
     fetch(url)
     .then(response => {
@@ -154,61 +132,77 @@ function getWeather(lat, long, itemID, date) {
             throw new Error(response.statusText);
         }
     })
-    .then(responseJson => displayWeather(responseJson,itemID,date))
+    .then(responseJson => displayWeather(responseJson,itemID,day))
     .catch(error => {$('.error-message').text(`Something went wrong getting weather: ${error.message}`);
     $('.error-message').removeClass('hidden')});
-
 }
 
 function upgradeMessage(itemID,daysToGo) {
     $(`li[id=${itemID}]`).append(`<div class="removable weather">
     <p>Upgrade to get weather for events up to 30 days out!</p>
     <img src="http://openweathermap.org/img/wn/11d@2x.png" alt="weather icon matching description">
-    <p> Event is over ${Math.floor(daysToGo)} days away</p>
+    <p> Event is ${Math.floor(daysToGo)} days away</p>
     <button type="submit" class="wHide">Hide</button>
     </div>`);
 }
 
 function check7(date) {
-    //event date minus current date => index of .daily
-    // day = 0 => current day
-    let dateISO = date+'T12:00:00.000Z';
+    let dateISO = date+'T12:00:00.000Z'; // puts event date into ISO format so Date() can be used to make calendar math easy
     let date3 = new Date(dateISO);
 
-    let now = new Date();
-    let diff = (date3 - now);
-    console.log('diff is ' + diff);
-    let diffDays = diff/86.4e6;
-    console.log('diff in days ' + diffDays);
+    let now = new Date(); // grabs current date
+    let diff = (date3 - now); // finds the number of milliseconds between the event date and current date
 
-    if (diffDays < 7 ) {
-        return [true];
+    //console.log('diff is ' + diff);
+    diff = diff/86.4e6; // milliseconds to days conversion
+    //console.log('diff in days ' + diff);
+    /*
+    dayIndex is eventually passed to displayWeather and is used to select the correct index of responseJson.daily
+    responseJson.daily is an array of length 8. Index 0 has weather for today and index 7 has weather for the 
+    day that is 7 days away
+    */
+    let dayIndex = Math.floor(diff+1);
+
+    if (diff < 7 ) { // If event is < 7 days out 
+        return [true, dayIndex];
     } else {
-        return [false, diffDays];
+        return [false, dayIndex];
     }
     }
     
+function hideWeather() {
+    $('ul').on('click',"button[class='wHide']", event => {
+        event.preventDefault();
+        $(event.currentTarget).parent().remove();
+    });
+
+}
+        
 function weather() {
     $('ul').on('click',"button[class='event']", event => {
         event.preventDefault();
         let lat = $(event.currentTarget).prev().children('.lat').text();
         let long = $(event.currentTarget).prev().children('.long').text();
         let itemID = $(event.currentTarget).parent().parent().attr('id');
-        let date = $(event.currentTarget).siblings('.date').children().text();
 
-        if ($(`li[id=${itemID}]`).children('.weather').length == 0) {
-            if (check7(date)[0]) {            
+        let eventDate = $(event.currentTarget).siblings('.date').children().text();
+        let dayDiff = check7(eventDate);
+
+        // If the li element contains no elements of class weather
+        // prevents multiple forcasts for same event
+        if ($(`li[id=${itemID}]`).children('.weather').length == 0) { 
+            if (dayDiff[0]) {  // If event is < 7 days out          
                 console.log(lat + long + itemID);
-                getWeather(lat,long,itemID,date);
+                getWeather(lat,long,itemID,dayDiff[1]);
             } else {
-                //display days to go and prompt to upgrade
-                upgradeMessage(itemID,check7(date)[1]);
+                //prompt to upgrade and display days to go
+                upgradeMessage(itemID,dayDiff[1]);
             }
         }
     });
 }
 
-// -------------------------------------------- End event management functions
+// -------------------------------------------- End weather management functions
 
 function masterFunction() {
     search();
